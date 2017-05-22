8652c358ea1019fe4acf702e319cf38262f95404
Panda <jsondotload@pandabian>
2017-05-22 00:24:46 -0700

    Add more python dependencies

--- install/pip.sh
+++ install/pip.sh
@@ -5,4 +5,19 @@ sudo pip install -U numpy
 sudo pip install -U scipy
 sudo pip install -U scikit-learn
 sudo pip install -U pandas
+sudo pip install -U matplotlib
+sudo pip install -U seaborn
+sudo pip install -U https://github.com/Theano/Theano/archive/master.zip
+sudo pip install -U https://github.com/Lasagne/Lasagne/archive/master.zip
+sudo pip install -U keras
+sudo pip install -U Flask
+sudo pip install -U -m pyaudio
+install-package libsamplerate
+sudo pip install -U scikits.samplerate
+sudo pip install -U audioread
+sudo pip install -U librosa
+sudo pip install -U madmom
+sudo pip install -U pretty_midi
+sudo pip install -U music21
+sudo pip install -U jupyter
 sudo pip install -U requests
