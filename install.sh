4922a44c7e82bade8207ccaec7cca0768e267b05
Panda <jsondotload@pandabian>
2017-05-21 19:39:47 -0700

    Add basic setup and Vim configurations

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Author: jsondotload
+
+# Get current dir and other useful dirs
+export DOTFILES_DIR BACKUP_DIR APPCFG_DIR SYSCFG_DIR WORKSPACE_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BACKUP_DIR="$HOME/.original_dotfiles"
+APPCFG_DIR="$DOTFILES_DIR/appcfg"
+SYSCFG_DIR="$DOTFILES_DIR/system"
+INSTALL_DIR="$DOTFILES_DIR/install"
+WORKSPACE_DIR="/tmp/dotfiles-workspace"
+
+# Create workspace
+mkdir -p $WORKSPACE_DIR
+
+# Include common functions
+. "$SYSCFG_DIR/.function"
+
+# Get current platform
+export OS DISTR ARCH VER
+get-platform OS DISTR ARCH VER
+
+# Ask to authenticate sudo access so sudo doesn't have to be asked later
+sudo echo "Got sudo access!"
+
+# Install packages
+install-package curl
+install-package git
+install-package tmux
+
+# Update dotfiles itself
+#if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+
+# Run install scripts
+. $INSTALL_DIR/vim.sh
+
+
+# Install dotfiles
+install-dotfile "$APPCFG_DIR/.vimrc"
+
+# Delete workspace
+rm -rf $WORKSPACE_DIR
---
+++ install/vim.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install vim proper
+install-package vim
+
+# Install apt-vim
+curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
+source ~/.bashrc || source ~/.bash_profile
+
+# Install colors
+VIVIDCHALK_DIR="$WORKSPACE_DIR/vim-vividchalk"
+mkdir -p $HOME/.vim/colors && \
+git clone https://github.com/tpope/vim-vividchalk.git $VIVIDCHALK_DIR
+mv $VIVIDCHALK_DIR/colors/* $HOME/.vim/colors/
+
+# Install vim extensions
+apt-vim install -y https://github.com/scrooloose/nerdtree.git
